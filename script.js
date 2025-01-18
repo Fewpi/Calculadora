1cffe4d310fc463765b6d69d717125b4c8275387
Felipe Lourenço <felipelima0301@gmail.com>
2025-01-18 13:50:45 -0300

    JS update

--- script.js
+++ script.js
@@ -4,25 +4,41 @@ let operadores = document.querySelectorAll('.operator')
 
 let numberOne = 0
 let numberTwo = 0
-let operador 
+let indicador = false
+let operador = ''
 
 
 operadores.forEach(operador => {
 operador.addEventListener('click', () => {
-    return(operador = true)
-  
+    operacao(operador)
+    indicador = true
+    
 })
 
+function operacao(operador){
+    console.log(indicador)
+    
+}
+
+
+
 })
+
+function calculo(number){
+    if(indicador === false){
+        numberOne += (number.textContent)
+        }else{
+            numberTwo += (number.textContent)
+        }
+   console.log(numberOne, numberTwo)
+}
+
 
 numbers.forEach(number => {
     number.addEventListener('click', () => {       
-        if(operador === false){
-        numberOne = (parseFloat(number.textContent))
-        }else{
-            numberTwo = (parseFloat(number.textContent))
-        }
+        calculo(number)
         
-        console.log(numberOne, numberTwo)
     })
 })
+
+
