bc1a0ec5ed047aa6d2aa55e2285f87bfc71baf22
Felipe Lourenço <felipelima0301@gmail.com>
2025-01-24 17:53:52 -0300

    project finnalized

--- script.js
+++ script.js
@@ -6,26 +6,45 @@ let calcule = document.querySelector('.calculo')
 
 let tela = document.querySelector('.screen')
 
+let clear = document.querySelector('.clean')
+
 let numberOne = 0
 let numberTwo = 0
 let indicador = false
 let operadorar
 let result 
+let calculoOk
 
 operadores.forEach(operador => {
 operador.addEventListener('click', () => {
     oper(operador.textContent)
+    })
+})
+
+
+numbers.forEach(number => {
+    number.addEventListener('click', () =>{ if(!calculoOk){calculo(number)
+        }
+    })
+})
+
+calcule.addEventListener('click', ()=>{
+    solution(numberOne, numberTwo, operadorar)
+    calculoOk = true
+})
+
+clear.addEventListener('click', () => {
+    limpar()
 })
 
 function oper(operador){
     indicador = true
     operadorar = operador
-    console.log(operador)
+    tela.textContent = operadorar
 }
 
-})
-
 function calculo(number){
+
     if(indicador === false){
         numberOne += (number.textContent)
         tela.textContent = numberOne
@@ -33,24 +52,11 @@ function calculo(number){
             numberTwo += (number.textContent)
             tela.textContent = numberTwo
         }
-   console.log(numberOne, numberTwo)
 }
 
 
-numbers.forEach(number => {
-    number.addEventListener('click', () => {       
-        calculo(number)
-        
-    })
-})
-
-calcule.addEventListener('click', ()=>{
-    solution(numberOne, numberTwo, operadorar)
-})
-
 function solution(numberOne, numberTwo, operadorar){
     if(result !== null){
-
    
     numberOne = parseFloat(numberOne)
     numberTwo = parseFloat(numberTwo)
@@ -72,6 +78,16 @@ function solution(numberOne, numberTwo, operadorar){
   
     tela.textContent = result
     }
+
+}
+
+function limpar(){
+    tela.textContent = 0
+    numberOne = 0
+    numberTwo = 0
+    indicador = false
+    calculoOk = false   
+    calculo
 }
 
     
