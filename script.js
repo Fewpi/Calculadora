76ee5bc71092b4964c007440758dff1e07c7bf22
Felipe Lourenço <felipelima0301@gmail.com>
2025-01-23 21:09:59 -0300

    fail try of JS

--- script.js
+++ script.js
@@ -2,33 +2,36 @@ let numbers = document.querySelectorAll('.number')
 
 let operadores = document.querySelectorAll('.operator')
 
+let calcule = document.querySelector('.calculo')
+
+let tela = document.querySelector('.screen')
+
 let numberOne = 0
 let numberTwo = 0
 let indicador = false
-let operador = ''
-
+let operadorar
+let result 
 
 operadores.forEach(operador => {
 operador.addEventListener('click', () => {
-    operacao(operador)
-    indicador = true
-    
+    oper(operador.textContent)
 })
 
-function operacao(operador){
-    console.log(indicador)
-    
+function oper(operador){
+    indicador = true
+    operadorar = operador
+    console.log(operador)
 }
-
-
 
 })
 
 function calculo(number){
     if(indicador === false){
         numberOne += (number.textContent)
+        tela.textContent = numberOne
         }else{
             numberTwo += (number.textContent)
+            tela.textContent = numberTwo
         }
    console.log(numberOne, numberTwo)
 }
@@ -41,4 +44,34 @@ numbers.forEach(number => {
     })
 })
 
+calcule.addEventListener('click', ()=>{
+    solution(numberOne, numberTwo, operadorar)
+})
 
+function solution(numberOne, numberTwo, operadorar){
+    if(result !== null){
+
+   
+    numberOne = parseFloat(numberOne)
+    numberTwo = parseFloat(numberTwo)
+
+    switch(operadorar){
+        case "+":
+            result = numberOne + numberTwo
+            break
+        case "-":
+            result = numberOne - numberTwo
+            break
+        case "*":
+            result = numberOne * numberTwo
+            break
+        case "/":
+            result = numberOne / numberTwo
+            break
+    }
+  
+    tela.textContent = result
+    }
+}
+
+    
